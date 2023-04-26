b960eae26a138e1490aebbf1140cc4c323aa09be
Hoàng Kiên <kienhv@appotapay.com>
2023-04-26 23:15:18 +0700

    add custumer guard

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomerResource\Pages;
+use App\Filament\Resources\CustomerResource\RelationManagers;
+use App\Models\Customer;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->label(trans('filament-user::user.resource.name')),
+                TextInput::make('phone')->required()->label(trans('phone'))->numeric(),
+                Forms\Components\TextInput::make('password')->label(trans('filament-user::user.resource.password'))
+                    ->password()
+                    ->maxLength(255)
+                    ->dehydrateStateUsing(static function ($state) use ($form){
+                        if(!empty($state)){
+                            return Hash::make($state);
+                        }
+
+                        $user = User::find($form->getColumns());
+                        if($user){
+                            return $user->password;
+                        }
+                    }),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable()->label(trans('filament-user::user.resource.id')),
+                TextColumn::make('name')->sortable()->searchable()->label(trans('filament-user::user.resource.name')),
+                TextColumn::make('phone')->sortable()->searchable()->label(trans('phone')),
+                Tables\Columns\TextColumn::make('created_at')->label(trans('filament-user::user.resource.created_at'))
+                    ->dateTime('M j, Y')->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')->label(trans('filament-user::user.resource.updated_at'))
+                    ->dateTime('M j, Y')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }    
+}
